30a4993756a6bae8f608879812891586f103710e
ducduong263 <116534248+ducduong263@users.noreply.github.com>
2023-11-10 10:37:07 +0700

    update

--- DoAnPhanMem/Models/Model1.Context.cs
+++ DoAnPhanMem/Models/Model1.Context.cs
@@ -19,12 +19,12 @@ namespace DoAnPhanMem.Models
             : base("name=WebshopEntities")
         {
         }
-    
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
         }
-    
+
         public virtual DbSet<Account> Accounts { get; set; }
         public virtual DbSet<AccountAddress> AccountAddresses { get; set; }
         public virtual DbSet<Brand> Brands { get; set; }
